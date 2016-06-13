3ad400ca95b10e499b57409f7a3a057d3c2cb884
Aditya Gnaneshwar <Aditya Gnaneshwar>
2016-06-12 21:11:42 -0700

    blah

---
+++ module4/js/script.js
@@ -0,0 +1,36 @@
+/*var x;
+x =5;
+for ( i=0; i<10;i++)
+	console.log(x);
+if (x==undefined)
+{
+	console.log("X is undefined");
+}
+else
+{
+	console.log(x);
+}
+var str = "hello";
+str+= " world!";
+console.log(str);
+x=4;
+var y=4;
+if (x===y)
+{
+	console.log("Both are equal");
+}
+else
+	console.log("Not Equal");
+console.log((5+4)%3);
+console.log((5+4)/3);
+
+
+function test()
+{
+	console.log("hello Coursera!");
+	console.log(this);
+	this.myName = "Aditya";
+}
+test();
+console.log(window.myName);*/
+
